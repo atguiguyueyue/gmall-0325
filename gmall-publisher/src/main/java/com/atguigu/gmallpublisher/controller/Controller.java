cd5544c936650987169251244bfa7a62b5e6d6c0
SH0325 <SH0325@atguigu.com>
2021-08-01 10:13:12 +0800

    day03-交易额需求

--- gmall-publisher/src/main/java/com/atguigu/gmallpublisher/controller/Controller.java
+++ gmall-publisher/src/main/java/com/atguigu/gmallpublisher/controller/Controller.java
@@ -34,18 +34,27 @@ public class Controller {
         //存放新增设备的map集合
         HashMap<String, Object> devMap = new HashMap<>();
 
+        //存放GMV的map集合
+        HashMap<String, Object> gmvMap = new HashMap<>();
+
         //4.将数据封装到Map集合中
         dauMap.put("id", "dau");
         dauMap.put("name", "新增日活");
-        dauMap.put("value",dauTotal);
+        dauMap.put("value", dauTotal);
 
         devMap.put("id", "new_mid");
         devMap.put("name", "新增设备");
         devMap.put("value", 233);
 
+        gmvMap.put("id", "order_amount");
+        gmvMap.put("name", "新增交易额");
+        gmvMap.put("value", publisherService.getSelectGmvTotal(date));
+
+
         //5.将map集合放入List集合
         result.add(dauMap);
         result.add(devMap);
+        result.add(gmvMap);
 
         return JSONObject.toJSONString(result);
     }
@@ -53,13 +62,27 @@ public class Controller {
     //获取分时数据方法
     @RequestMapping("realtime-hours")
     public String getDauHour(@RequestParam("id") String id,
-                             @RequestParam("date") String date){
+                             @RequestParam("date") String date) {
         //1.获取service返回的数据
         String yesterday = LocalDate.parse(date).plusDays(-1).toString();
-        //1.1获取今天的数据
-        Map todayMap = publisherService.getSelectDauHour(date);
-        //1.2获取昨天的数据
-        Map yesterdayMap = publisherService.getSelectDauHour(yesterday);
+
+        Map todayMap=null;
+        Map yesterdayMap=null;
+
+        //根据id来判断获取的是谁的数据
+        if ("order_amount".equals(id)) {
+            //获取gmv的分时数据
+            //1.1获取今天的数据
+            todayMap = publisherService.getSelectGmvHour(date);
+            //1.2获取昨天的数据
+            yesterdayMap = publisherService.getSelectGmvHour(yesterday);
+        }else {
+            //获取日活的分时数据
+            //1.1获取今天的数据
+            todayMap = publisherService.getSelectDauHour(date);
+            //1.2获取昨天的数据
+            yesterdayMap = publisherService.getSelectDauHour(yesterday);
+        }
 
         //2.创建存放最终结果的map集合
         HashMap<String, Map> result = new HashMap<>();
--- gmall-publisher/src/main/java/com/atguigu/gmallpublisher/service/PublisherService.java
+++ gmall-publisher/src/main/java/com/atguigu/gmallpublisher/service/PublisherService.java
@@ -8,4 +8,10 @@ public interface PublisherService {
 
     //获取分时数据抽象方法
     public Map getSelectDauHour(String date);
+
+    //获取GMV每日总数的抽象方法
+    public Double getSelectGmvTotal(String date);
+
+    //获取GMV分时数据的抽象方法
+    public Map getSelectGmvHour(String date);
 }
